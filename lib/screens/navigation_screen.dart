8018941c06513bf4338af17b6fc520b717cbc35b
OnyemaAnthony <Anthonychibuike246@gmail.com>
2021-08-13 15:33:19 +0000

    make appbar responsive web and mobile

--- lib/screens/navigation_screen.dart
+++ lib/screens/navigation_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:netflix/screens/Home_screen.dart';
+import 'package:netflix/widgets/responsive.dart';
 
 class NavigationScreen extends StatefulWidget {
   @override
@@ -7,49 +8,51 @@ class NavigationScreen extends StatefulWidget {
 }
 
 class _NavigationScreenState extends State<NavigationScreen> {
-  final List<Widget> _screens=[
-    HomeScreen(key:PageStorageKey('HomeScreen')),
+  final List<Widget> _screens = [
+    HomeScreen(key: PageStorageKey('HomeScreen')),
     Scaffold(),
     Scaffold(),
     Scaffold(),
     Scaffold(),
   ];
 
-  final Map<String,IconData> _icons = {
-    'Home':Icons.home,
-    'Search':Icons.search,
-    'Comming Soon':Icons.queue_play_next,
-    'Downloads':Icons.file_download,
-    'More':Icons.menu,
+  final Map<String, IconData> _icons = {
+    'Home': Icons.home,
+    'Search': Icons.search,
+    'Comming Soon': Icons.queue_play_next,
+    'Downloads': Icons.file_download,
+    'More': Icons.menu,
   };
 
   int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _screens[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
+      bottomNavigationBar: !Responsive.isDesktop(context) ? BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         backgroundColor: Colors.black,
 
-        items: _icons.map((title, icon) => MapEntry(title, BottomNavigationBarItem(
-          icon: Icon(icon,size: 30,),
-          label:title
-        )
+        items: _icons.map((title, icon) =>
+            MapEntry(title, BottomNavigationBarItem(
+                icon: Icon(icon, size: 30,),
+                label: title
+            ),
 
-        )).values.toList(),
+            ),).values.toList(),
         currentIndex: _currentIndex,
         selectedItemColor: Colors.white,
         unselectedFontSize: 11,
         selectedFontSize: 11,
         unselectedItemColor: Colors.grey,
-        onTap: (index){
+        onTap: (index) {
           setState(() {
             _currentIndex = index;
           });
         },
 
-      ),
+      ):null,
     );
   }
 }
--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:netflix/utilities.dart';
+import 'package:netflix/widgets/responsive.dart';
 
 class CustomAppBar extends StatelessWidget {
   final double? scrollOffset;
@@ -13,35 +14,11 @@ class CustomAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(vertical: 10, horizontal: 24),
-      color: Colors.black.withOpacity((scrollOffset! / 350).clamp(0, 1).toDouble()),
-      child: SafeArea(
-        child: Row(
-          children: [
-            Image.asset(Utilities.netflixLogo0),
-            SizedBox(
-              width: 12,
-            ),
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  AppBarButton(
-                    title: "Tv Shows",
-                    onTap: () {},
-                  ),
-                  AppBarButton(
-                    title: "Movies",
-                    onTap: () {},
-                  ),
-                  AppBarButton(
-                    title: "My List",
-                    onTap: () {},
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+      color: Colors.black
+          .withOpacity((scrollOffset! / 350).clamp(0, 1).toDouble()),
+      child: Responsive(
+        mobile: _CustomAppBarMobile(),
+        desktop: _CustomAppBarDesktop(),
       ),
     );
   }
@@ -65,6 +42,121 @@ class AppBarButton extends StatelessWidget {
         title!,
         style: TextStyle(
             color: Colors.white, fontSize: 16, fontWeight: FontWeight.w600),
+      ),
+    );
+  }
+}
+
+class _CustomAppBarMobile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Row(
+        children: [
+          Image.asset(Utilities.netflixLogo0),
+          SizedBox(
+            width: 12,
+          ),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                AppBarButton(
+                  title: "Tv Shows",
+                  onTap: () {},
+                ),
+                AppBarButton(
+                  title: "Movies",
+                  onTap: () {},
+                ),
+                AppBarButton(
+                  title: "My List",
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBarDesktop extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Row(
+        children: [
+          Image.asset(Utilities.netflixLogo1),
+          SizedBox(
+            width: 12,
+          ),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                AppBarButton(
+                  title: "Home",
+                  onTap: () {},
+                ),
+                AppBarButton(
+                  title: "Tv Shows",
+                  onTap: () {},
+                ),
+                AppBarButton(
+                  title: "Movies",
+                  onTap: () {},
+                ),
+                AppBarButton(
+                  title: "My List",
+                  onTap: () {},
+                ),
+                AppBarButton(
+                  title: "Latest",
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+          Spacer(),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.search),
+                  iconSize: 28,
+                  color: Colors.white,
+                  padding: EdgeInsets.zero,
+                ),
+                AppBarButton(
+                  title: "Kids",
+                  onTap: () {},
+                ),
+                AppBarButton(
+                  title: "DVD",
+                  onTap: () {},
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.card_giftcard),
+                  iconSize: 28,
+                  color: Colors.white,
+                  padding: EdgeInsets.zero,
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.notifications ),
+                  iconSize: 28,
+                  color: Colors.white,
+                  padding: EdgeInsets.zero,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
