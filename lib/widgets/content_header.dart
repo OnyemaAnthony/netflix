6453b72981723852f5b8525d07078d58761bcd65
OnyemaAnthony <Anthonychibuike246@gmail.com>
2021-08-13 15:51:23 +0000

    play video on web

--- lib/widgets/content_header.dart
+++ lib/widgets/content_header.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:netflix/models/content.dart';
+import 'package:netflix/widgets/responsive.dart';
 import 'package:netflix/widgets/vertical_icon_button.dart';
+import 'package:video_player/video_player.dart';
 
 class ContentHeader extends StatelessWidget {
   final Content? featuredContent;
 
   const ContentHeader({
+    Key? key,
+    this.featuredContent,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Responsive(
+      mobile: ContentHeaderMobile(featuredContent: featuredContent),
+      desktop: ContentHeaderDesktop(featuredContent: featuredContent),
+    );
+  }
+}
+
+class ContentHeaderMobile extends StatelessWidget {
+  final Content? featuredContent;
+
+  const ContentHeaderMobile({
     Key? key,
     this.featuredContent,
   }) : super(key: key);
@@ -60,12 +79,108 @@ class ContentHeader extends StatelessWidget {
   }
 }
 
+class ContentHeaderDesktop extends StatefulWidget {
+  final Content? featuredContent;
+
+  const ContentHeaderDesktop({
+    Key? key,
+    this.featuredContent,
+  }) : super(key: key);
+
+  @override
+  _ContentHeaderDesktopState createState() => _ContentHeaderDesktopState();
+}
+
+class _ContentHeaderDesktopState extends State<ContentHeaderDesktop> {
+  late VideoPlayerController controller;
+  bool isMuted = true;
+
+  @override
+  void initState() {
+    controller =
+        VideoPlayerController.network(widget.featuredContent!.videoUrl!)
+          ..initialize().then((value) {
+            setState(() {});
+          })
+          ..play();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        controller.value.isPlaying ? controller.pause() : controller.play();
+      },
+      child: Stack(
+        alignment: Alignment.bottomLeft,
+        children: [
+          Container(
+            height: 500,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage(widget.featuredContent!.imageUrl!),
+              fit: BoxFit.cover,
+            )),
+          ),
+          Container(
+            height: 500,
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+              colors: [Colors.black, Colors.transparent],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+            )),
+          ),
+          Positioned(
+            left: 60,
+            right: 60,
+            bottom: 150,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: 250,
+                  child: Image.asset(widget.featuredContent!.titleImageUrl!),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  widget.featuredContent!.description!,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    shadows: [
+                      Shadow(
+                          color: Colors.black,
+                          offset: Offset(2, 4),
+                          blurRadius: 6),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 class _PlayButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FlatButton.icon(
       color: Colors.white,
-      padding: EdgeInsets.fromLTRB(15, 5, 20, 5 ),
+      padding: EdgeInsets.fromLTRB(15, 5, 20, 5),
       onPressed: () {},
       icon: Icon(
         Icons.play_arrow,
